50765675421fa31aab6afe3be6b55b7fcc510256
Jürgen Viljaste <j.viljaste@gmail.com>
2015-04-20 20:27:46 +0300

    Update

--- src/teamspeak/build/modules/teamspeak/manifests/teamspeak.pp
+++ src/teamspeak/build/modules/teamspeak/manifests/teamspeak.pp
@@ -17,4 +17,8 @@ class teamspeak::teamspeak {
   bash_exec { 'rm -rf /usr/local/src/teamspeak3-server/files':
     require => Bash_exec['mv /tmp/teamspeak3-server_linux-amd64 /usr/local/src/teamspeak3-server']
   }
+
+  bash_exec { 'mkdir -p /usr/local/src/teamspeak3-server/logs':
+    require => Bash_exec['mv /tmp/teamspeak3-server_linux-amd64 /usr/local/src/teamspeak3-server']
+  }
 }
